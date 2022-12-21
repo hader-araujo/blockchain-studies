caa6688a6dab4c1641b69aa372d6a1502a9e4d37
Hader <hader.araujo@gmail.com>
2022-12-21 14:06:29 -0300

    frontend

--- SimpleStorage/truffle-config.js
+++ SimpleStorage/truffle-config.js
@@ -58,6 +58,8 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
+  contracts_build_directory: './frontend/src/contracts',
+
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
@@ -100,7 +102,7 @@ module.exports = {
 
       development: {
         host: "127.0.0.1",     // Localhost (default: none)
-        port: 8545,            // Standard BSC port (default: none)
+        port: 9545,            // Standard BSC port (default: none)
         network_id: "*",       // Any network (default: none)
       },
       testnet: {
